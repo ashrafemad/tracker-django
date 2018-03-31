6b4c04c25609d81ec8fbcea55f8aa042c003ddfb
ashrafemad <ashrafemad23@gmail.com>
2018-03-31 05:06:58 +0200

    deploying

--- task/settings.py
+++ task/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django_heroku
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -131,6 +133,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
